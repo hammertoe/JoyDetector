a2d7a6a1e821f438f89fd7cc829bc2a03c42e9d4
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-06-16 12:47:01 +0100

    code tidy up

--- convert.py
+++ convert.py
@@ -13,8 +13,8 @@ import tensorflow as tf
 
 import numpy as np
 
-#tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
-
+# define our model, note: we can't just load a saved model as we trained it
+# with GPU versions of the LSTM, and need to use the non-GPU ones here
 model = Sequential()
 model.add(Embedding(472607, 100, input_length=56, trainable=False))
 model.add(LSTM(100, return_sequences=True))
@@ -22,44 +22,36 @@ model.add(Dropout(0.2))
 model.add(LSTM(100))
 model.add(Dropout(0.2))
 model.add(Dense(2, activation='softmax'))
-# compile the model                                                                                                                                                                                                                   
+
+# compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+# load the weights from our training run
 model.load_weights("tweet_model_weights.h5")
 
-
+# Extract the embedding matrix from the first layer
 embedding_matrix = model.layers[0].get_weights()[0]
 print( 'Embedding Shape ~> {}'.format( embedding_matrix.shape ) )
 
-#word_index = tokenizer.word_index
-#word_index_2 = dict()
-#for word , index in word_index.items():
-#    word_index_2[ index ] = word
-#word_index = word_index_2
-#embedding_dict = dict()
-
-
-#for i in range( len( embedding_matrix ) - 1 ):
-#    embedding_dict[ word_index[ i + 1 ] ] = embedding_matrix[ i + 1 ]
-
-
+# Save the embedding matrix for a compressed pickle
 joblib.dump(embedding_matrix, "embedding_matrix.pkl.bz2")
-#with open("embedding_matrix.pkl", "wb") as f:
-#    pickle.dump(embedding_matrix, f)
 
+# Create some dummy input
 input = np.zeros((1, 56, 100))
 
+# Create a new model, removing the embedding layer
 new_model = Sequential(model.layers[1:])
 new_model.build(input_shape=input.shape)
-#new_model.save("noembed_tweet_model.h5")
 
 # We need to do a prediction to get TF to setup the right shape
 new_model.predict(input)
 
+# Convert the model to tflite, enabling quantization to make
+# it a bit smaller
 converter = tf.lite.TFLiteConverter.from_keras_model(new_model)
 converter.post_training_quantize = True
 tflite_buffer = converter.convert()
+
+# Save the tflite file out
 with open( "tweet_model.tflite" , "wb" ) as f:
     f.write( tflite_buffer )
-
-#model.save("tweet_model.h5")
